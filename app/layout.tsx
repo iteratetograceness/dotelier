aab168ca367f526fa1e4a420530833e903e66fdd
Grace Yun <graceyunn@gmail.com>
2024-12-24 22:30:11 -0500

    Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from '@vercel/analytics/react'
 import { Toaster } from 'sonner'
 import { Tiny5 } from 'next/font/google'
 import './globals.css'
@@ -25,6 +26,7 @@ export default function RootLayout({
         <Header />
         {children}
         <Toaster />
+        <Analytics />
       </body>
     </html>
   )
