cbe6566aa2ad87a29eccb148404b6b714a06f9a5
Grace Yun <graceyunn@gmail.com>
2024-12-27 03:34:57 -0500

    Fix auth base url

--- app/db/client.ts
+++ app/db/client.ts
@@ -3,8 +3,15 @@ import createAuth from '@edgedb/auth-nextjs/app'
 
 export const db = createClient()
 
+const baseUrl =
+  process.env.VERCEL_ENV === 'production'
+    ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
+    : process.env.VERCEL_ENV === 'preview'
+    ? `https://${process.env.VERCEL_URL}`
+    : 'http://localhost:3000'
+
 export const auth = createAuth(db, {
-  baseUrl: process.env.VERCEL_URL || 'http://localhost:3000',
+  baseUrl,
   authCookieName: 'sesh',
   pkceVerifierCookieName: 'pkce',
 })
