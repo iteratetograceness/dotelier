4cd4cd074a252ea7c057b81b0b77a2f5dad5e5d3
Grace Yun <graceyunn@gmail.com>
2025-01-19 19:19:09 -0500

    suspenssy

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from 'react'
 import { Admin } from './_components/admin'
 import { PixelGenerator } from './_components/form'
 import { DraggableWindows } from './_components/window'
@@ -5,7 +6,9 @@ import { DraggableWindows } from './_components/window'
 export default function Home() {
   return (
     <>
-      <Admin />
+      <Suspense fallback={null}>
+        <Admin />
+      </Suspense>
       <DraggableWindows />
       <PixelGenerator />
     </>
