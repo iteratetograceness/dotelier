fcc5d655bd82250d91d8e52d501079be494ec315
Grace Yun <graceyunn@gmail.com>
2025-02-24 18:27:02 -0500

    codemode failure

--- app/atelier/page.tsx
+++ app/atelier/page.tsx
@@ -4,15 +4,12 @@ import {
 } from '@/app/_components/explore/with-user'
 import { Suspense } from 'react'
 
-export default async function MyIcons(
-  props: {
-    searchParams: Promise<{ p: string | null }>
-  }
-) {
-  const searchParams = await props.searchParams;
+export default async function MyIcons(props: {
+  searchParams: Promise<{ p: string | null }>
+}) {
   return (
     <Suspense fallback={<ExplorerWithUserSkeleton />}>
-      <ExplorerWithUser searchParams={searchParams} />
+      <ExplorerWithUser searchParams={props.searchParams} />
     </Suspense>
   )
 }
