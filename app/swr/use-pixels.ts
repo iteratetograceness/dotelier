39d5a4932ba7d64e840f017b5913a435833a3af6
Grace Yun <graceyunn@gmail.com>
2025-03-24 04:17:14 -0400

    type issue

--- app/swr/use-pixels.ts
+++ app/swr/use-pixels.ts
@@ -1,25 +1,24 @@
-import useSWRInfinite from 'swr/infinite'
-import { PixelLite } from '../db/supabase/types'
-import { fetcher } from './shared'
+// import useSWRInfinite from 'swr/infinite'
+// import { fetcher } from './shared'
 
-const getKey = (page: number, previousPageData: PixelLite[]) => {
-  if (previousPageData && !previousPageData.length) return null
-  return `/api/pixels?p=${page}`
-}
+// const getKey = (page: number, previousPageData) => {
+//   if (previousPageData && !previousPageData.length) return null
+//   return `/api/pixels?p=${page}`
+// }
 
-export function usePixelsInfinite() {
-  const { data, error, isLoading, mutate, size, setSize } = useSWRInfinite<
-    PixelLite[]
-  >(getKey, fetcher, { parallel: true })
+// export function usePixelsInfinite() {
+//   const { data, error, isLoading, mutate, size, setSize } = useSWRInfinite(
+//     getKey,
+//     fetcher,
+//     { parallel: true }
+//   )
 
-  console.log('data', data)
-
-  return {
-    pixels: data,
-    isLoading,
-    isError: error,
-    mutate,
-    pages: size,
-    setPages: setSize,
-  }
-}
+//   return {
+//     pixels: data,
+//     isLoading,
+//     isError: error,
+//     mutate,
+//     pages: size,
+//     setPages: setSize,
+//   }
+// }
