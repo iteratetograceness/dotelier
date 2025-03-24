6190c1b45aa0ef3f6c4885908d3e43292500da08
Grace Yun <graceyunn@gmail.com>
2025-03-24 04:38:54 -0400

    wwwwwww

--- lib/base-url.ts
+++ lib/base-url.ts
@@ -1,9 +1,9 @@
 export function getBaseUrl(path?: string) {
   const baseUrl =
     process.env.NEXT_PUBLIC_VERCEL_ENV === 'production'
-      ? 'https://dotelier.studio'
+      ? 'https://www.dotelier.studio'
       : process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview'
-      ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+      ? `https://www.${process.env.NEXT_PUBLIC_VERCEL_URL}`
       : 'http://localhost:3000'
   return path ? `${baseUrl}${path}` : baseUrl
 }
