a046e44e953678c077bb08265cb3d3c1fcd19899
Grace Yun <graceyunn@gmail.com>
2025-04-05 17:12:06 -0400

    feat: warm up in layout

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { StudioServer } from './_components/studio/server'
 import { StudioSkeleton } from './_components/studio/skeleton'
 
 export default function Home() {
+
   return (
     <div className='py-7 md:my-auto'>
       <Suspense fallback={<StudioSkeleton />}>
