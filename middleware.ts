716774bfa44840ae52889df5c732d3c75e55bee8
Grace Yun <graceyunn@gmail.com>
2025-04-06 17:43:26 -0400

    admin middleware

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,14 @@
 import type { NextRequest } from 'next/server'
 import { NextResponse } from 'next/server'
+import { getSession } from './lib/auth/session'
 
 export async function middleware(request: NextRequest) {
-  // if (request.nextUrl.pathname.includes('/admin')) {
-  //   return adminMiddleware(request)
-  // }
+  if (request.nextUrl.pathname.includes('/admin')) {
+    const session = await getSession()
+    if (!session || session.user.role !== 'admin') {
+      return NextResponse.redirect(new URL('/', request.url))
+    }
+  }
 
   return NextResponse.next()
 }
